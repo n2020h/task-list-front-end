e7faa046988bc887505b7c06477869fbc8d4b525
kathawkins <kmoreno1205@gmail.com>
2022-12-13 14:52:48 -0700

    Added comments for clarity

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,14 +3,18 @@ import PropTypes from 'prop-types';
 
 import './Task.css';
 
+//  Using Task props destructuring:
 const Task = ({ id, title, isComplete }) => {
   const [complete, setComplete] = useState(isComplete);
+  // Using ? as ternary operator:
+  // conditionToTest ? valueIfTrue : valueIfFalse
   const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
 
   return (
     <li className="tasks__item">
       <button
         className={`tasks__item__toggle ${buttonClass}`}
+        // Using the ! NOT operator to update state with the event listener:
         onClick={() => setComplete(!complete)}
       >
         {title}
